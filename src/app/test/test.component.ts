ae33a9915a9e52438246c0d1ca960aad41876c88
Manish <19luckyreddy@gmail.com>
2023-01-23 13:50:22 +0530

    init

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -6,6 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./test.component.css'],
 })
 export class TestComponent {
-  countries = ["India", "Malaysia", "Singapore", "Hongkong"];
-  employees = [{ "empno": 1231, "empname": "Thangaraj S", "age": 44, "salary": 34567.7734 }]
+  employees = [
+    { "empno": 1231, "empname": "Thangaraj S", "age": 44, "salary": 34567.7734 },
+    { "empno": 1232, "empname": "Michel", "age": 39, "salary": 34567.7734 },
+    { "empno": 1233, "empname": "Muzeeb", "age": 32, "salary": 34567.7734 },
+    { "empno": 1234, "empname": "Archan", "age": 45, "salary": 34567.7734 },
+    { "empno": 1235, "empname": "Ishant", "age": 49, "salary": 34567.7734 },
+    { "empno": 1236, "empname": "Herosh", "age": 51, "salary": 34567.7734 },
+    { "empno": 1237, "empname": "Hemanth", "age": 48, "salary": 34567.7734 }
+  ]
 }
